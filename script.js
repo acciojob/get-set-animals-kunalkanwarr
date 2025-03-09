90d26593821a7c55b82f3b922af3fe950f518c99
Kunal Kanwar <113274734+kunalkanwarr@users.noreply.github.com>
2025-03-09 11:22:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-class Animal {
-  constructor(private _species: string) {}
+// class Animal {
+//   constructor(private _species: string) {}
 
-  get species(): string {
-    return this._species;
-  }
+//   get species(): string {
+//     return this._species;
+//   }
 
-  makeSound(): void {
-    console.log(`The ${this._species} makes a sound`);
-  }
-}
+//   makeSound(): void {
+//     console.log(`The ${this._species} makes a sound`);
+//   }
+// }
 
-class Cat extends Animal {
-  constructor(species: string) {
-    super(species);
-  }
+// class Cat extends Animal {
+//   constructor(species: string) {
+//     super(species);
+//   }
 
-  purr(): void {
-    console.log("purr");
-  }
-}
+//   purr(): void {
+//     console.log("purr");
+//   }
+// }
 
-class Dog extends Animal {
-  constructor(species: string) {
-    super(species);
-  }
+// class Dog extends Animal {
+//   constructor(species: string) {
+//     super(species);
+//   }
 
-  bark(): void {
-    console.log("woof");
-  }
-}
+//   bark(): void {
+//     console.log("woof");
+//   }
+// }
 
-// Example Usage
-const myCat = new Cat("Siamese");
-myCat.makeSound(); // Expected Output: The Siamese makes a sound
-myCat.purr(); // Expected Output: purr
+// // Example Usage
+// const myCat = new Cat("Siamese");
+// myCat.makeSound(); // Expected Output: The Siamese makes a sound
+// myCat.purr(); // Expected Output: purr
 
-const myDog = new Dog("Golden Retriever");
-myDog.makeSound(); // Expected Output: The Golden Retriever makes a sound
-myDog.bark(); // Expected Output: woof
+// const myDog = new Dog("Golden Retriever");
+// myDog.makeSound(); // Expected Output: The Golden Retriever makes a sound
+// myDog.bark(); // Expected Output: woof
