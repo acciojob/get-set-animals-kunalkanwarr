c663049ae12c288c253322a5bd4fe5c10a260b8a
Kunal Kanwar <113274734+kunalkanwarr@users.noreply.github.com>
2025-03-09 11:17:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(private _species: string) {}
 
-class Dog extends Animal {}
+  get species(): string {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  makeSound(): void {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+class Cat extends Animal {
+  constructor(species: string) {
+    super(species);
+  }
+
+  purr(): void {
+    console.log("purr");
+  }
+}
+
+class Dog extends Animal {
+  constructor(species: string) {
+    super(species);
+  }
+
+  bark(): void {
+    console.log("woof");
+  }
+}
+
+// Example Usage
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Expected Output: The Siamese makes a sound
+myCat.purr(); // Expected Output: purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Expected Output: The Golden Retriever makes a sound
+myDog.bark(); // Expected Output: woof
