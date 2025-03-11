45e607d5bd5ccf4b02b5fdb18862447fb9af0e1c
Kunal Kanwar <113274734+kunalkanwarr@users.noreply.github.com>
2025-03-11 12:13:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
 // class Animal {
 //   constructor(private _species: string) {}
+// Animal class
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
+  
+  get species() {
+    return this._species;
+  }
+  
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
+
+// Cat class
+class Cat extends Animal {
+  constructor(species) {
+    super(species);
+  }
+  
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Dog class
+class Dog extends Animal {
+  constructor(species) {
+    super(species);
+  }
+  
+  bark() {
+    console.log("woof");
+  }
+}
+
+// Example usage in HTML
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("createCat").addEventListener("click", () => {
+    const cat = new Cat("Siamese");
+    document.getElementById("output").innerText = `The ${cat.species} makes a sound`;
+    console.log("purr");
+  });
+
+  document.getElementById("createDog").addEventListener("click", () => {
+    const dog = new Dog("Golden Retriever");
+    document.getElementById("output").innerText = `The ${dog.species} makes a sound`;
+    console.log("woof");
+  });
+});
 
 //   get species(): string {
 //     return this._species;
